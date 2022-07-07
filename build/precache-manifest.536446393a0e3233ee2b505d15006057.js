5b91f3b9f0c5e8e66b9f81f470432933fb6b6e66
Andrey Loktionov <adloktionov@gmail.com>
2022-07-07 16:07:39 +0300

    добавлена папке билд в гитигноре

---
+++ build/precache-manifest.536446393a0e3233ee2b505d15006057.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "d68f502dd46edcb7addf",
+    "url": "/static/css/main.64f7edb4.chunk.css"
+  },
+  {
+    "revision": "d68f502dd46edcb7addf",
+    "url": "/static/js/main.d68f502d.chunk.js"
+  },
+  {
+    "revision": "d11720e887a1448ed361",
+    "url": "/static/js/1.d11720e8.chunk.js"
+  },
+  {
+    "revision": "509f2ec11a813eb3721c",
+    "url": "/static/css/2.aabad3cb.chunk.css"
+  },
+  {
+    "revision": "509f2ec11a813eb3721c",
+    "url": "/static/js/2.509f2ec1.chunk.js"
+  },
+  {
+    "revision": "28dc535bcaba80dd7765",
+    "url": "/static/js/runtime~main.28dc535b.js"
+  },
+  {
+    "revision": "eecca142ebfd12ef575ddd59b54a7f22",
+    "url": "/static/media/comfortaa-latin-300.eecca142.woff2"
+  },
+  {
+    "revision": "d847c938a3d7db7d775e64e7eb941df1",
+    "url": "/static/media/comfortaa-latin-300.d847c938.woff"
+  },
+  {
+    "revision": "5cb8925d8c45787b80a01f51020d8ab8",
+    "url": "/static/media/comfortaa-latin-400.5cb8925d.woff2"
+  },
+  {
+    "revision": "ac2c653a7f6f7433930bdbfeb6de7b6d",
+    "url": "/static/media/comfortaa-latin-400.ac2c653a.woff"
+  },
+  {
+    "revision": "ec5e1b66bbce3312b57282293251c2a7",
+    "url": "/static/media/comfortaa-latin-500.ec5e1b66.woff2"
+  },
+  {
+    "revision": "5bcf07291d4af3f3e5bf916eae1dc91d",
+    "url": "/static/media/comfortaa-latin-500.5bcf0729.woff"
+  },
+  {
+    "revision": "7ce44fb25d884bc334f360b071063259",
+    "url": "/static/media/comfortaa-latin-600.7ce44fb2.woff2"
+  },
+  {
+    "revision": "31205edd6895c3a2d6d99b891795741a",
+    "url": "/static/media/comfortaa-latin-600.31205edd.woff"
+  },
+  {
+    "revision": "08f0ac601469e08f15731854e70509d0",
+    "url": "/static/media/comfortaa-latin-700.08f0ac60.woff2"
+  },
+  {
+    "revision": "5448a7b477307bb07c735fe01a6b502d",
+    "url": "/static/media/comfortaa-latin-700.5448a7b4.woff"
+  },
+  {
+    "revision": "9df5f5abd7f933120de095b64e5cdb2a",
+    "url": "/index.html"
+  }
+];
